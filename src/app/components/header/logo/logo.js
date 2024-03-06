2bddc79077d7857cd45d2b3306e33666cbc5f218
ansen01-svg <ansenbeyc73@gmail.com>
2024-03-06 10:53:38 +0530

    resolving conflict

--- src/app/components/header/logo/logo.js
+++ src/app/components/header/logo/logo.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../../../assets/logo.png";
+import appLogo from "../../../assets/logo.png";
 
 export default function Logo() {
   return (
     <div>
       <Link href={"/"}>
-        <Image src={logo} alt="logo" width={168} height={35} />
+        <Image src={appLogo} alt="logo" width={168} height={35} />
       </Link>
     </div>
   );
