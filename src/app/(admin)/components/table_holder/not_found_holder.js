7416b4e53f2f943c20dfb13a262cb3e59575cc5c
ansen01-svg <ansenbeyc73@gmail.com>
2024-05-16 09:32:12 +0530

    testing for admin route group pages data fetching problem

--- src/app/(admin)/components/table_holder/not_found_holder.js
+++ src/app/(admin)/components/table_holder/not_found_holder.js
@@ -2,39 +2,66 @@ import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import TableRowsOutlinedIcon from "@mui/icons-material/TableRowsOutlined";
 
-export default function NotFound() {
+export default function NotFound({ tableHeadValues }) {
   return (
     <TableRow
       sx={{
         "&:last-child td, &:last-child th": { border: 0 },
       }}
     >
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell
-        component="th"
-        scope="row"
-        sx={{
-          fontSize: "13px",
-          color: "rgba(36,39,44,.7)",
-          whiteSpace: "nowrap",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <TableRowsOutlinedIcon fontSize="large" />
-        No rows found
-      </TableCell>
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell component="th" scope="row"></TableCell>
-      <TableCell component="th" scope="row"></TableCell>
+      {tableHeadValues.map((value, index) => {
+        const even = tableHeadValues.length % 2 === 0;
+
+        // if length is even
+        if (even && index === tableHeadValues.length / 2 - 1) {
+          return (
+            <TableCell
+              key={value.id}
+              component="th"
+              scope="row"
+              sx={{
+                fontSize: "13px",
+                color: "rgba(36,39,44,.7)",
+                whiteSpace: "nowrap",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <TableRowsOutlinedIcon fontSize="large" />
+              No rows found
+            </TableCell>
+          );
+        }
+
+        // if length is odd
+        if (!even && index === Math.floor(tableHeadValues.length / 2)) {
+          return (
+            <TableCell
+              key={value.id}
+              component="th"
+              scope="row"
+              sx={{
+                fontSize: "13px",
+                color: "rgba(36,39,44,.7)",
+                whiteSpace: "nowrap",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <TableRowsOutlinedIcon fontSize="large" />
+              No rows found
+            </TableCell>
+          );
+        }
+
+        return (
+          <TableCell key={value.id} component="th" scope="row"></TableCell>
+        );
+      })}
     </TableRow>
   );
 }
