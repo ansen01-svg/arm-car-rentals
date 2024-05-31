bd84db55fe293df0f6278ae1d9ef4f95696cd961
ansen01-svg <ansenbeyc73@gmail.com>
2024-05-31 22:31:11 +0530

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import DateLocalizationProvider from "./components/date_localization_provider/date_localization_provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <div role="main">
           <DateLocalizationProvider>{children}</DateLocalizationProvider>
         </div>
+        <Analytics />
       </body>
     </html>
   );
